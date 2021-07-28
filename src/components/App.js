d755aa4061bd916841a25c416af50f2089572588
Elisa Shapiro <elisashapiro@gmail.com>
2021-07-27 20:31:49 -0400

    deliverable one

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PokemonPage from "./PokemonPage";
 
 function App() {
+  const [pokemonCards, setPokemonCards] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/pokemon")
+    .then(response => response.json())
+    .then(pokemonData => setPokemonCards(pokemonData))
+  }, [])
+
   return (
     <div className="App">
-      <PokemonPage />
+      <PokemonPage pokemonCards={pokemonCards}/>
     </div>
   );
 }
--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,16 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({ pokemonCards }) {
   return (
     <Card.Group itemsPerRow={6}>
       <h1>Hello From Pokemon Collection</h1>
+      {pokemonCards.map((pokemonCard) => {
+        return (<PokemonCard 
+          key={pokemonCard.id}
+          pokemonCard={pokemonCard}
+        />)
+      })}
     </Card.Group>
   );
 }
